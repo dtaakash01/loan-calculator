4408825a9bf64f1a761e0701d21d733bfb40139f
Akash Dixit <akashdixit@localhost.localdomain>
2021-05-05 18:45:59 +0530

    some commit

---
+++ app.js
@@ -0,0 +1,81 @@
+/* document.getElementById('loan-form').addEventListener('submit', calculateResults); */
+
+
+
+document.getElementById('loan-form').addEventListener('submit', function(e){
+
+    document.getElementById('results').style.display = 'none';
+
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+})
+
+function calculateResults(e){
+    console.log("Calculating...");
+
+    //UI vars
+    const amount = document.getElementById('amount');
+    const interests = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterests = parseFloat(interests.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value)*12;
+
+    // Monthly payment compute
+    const x = Math.pow(1 + calculatedInterests, calculatedPayments);
+    const monthly = (principal*x*calculatedInterests)/(x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = ((monthly*calculatedPayments)).toFixed(2);
+        totalInterest.value = ((monthly*calculatedPayments) - principal).toFixed(2);
+
+        document.getElementById('results').style.display = 'block';
+
+        document.getElementById('loading').style.display = 'none';
+    }
+    else {
+        showError("Please Enter values");
+    }
+
+    console.log("amount", principal);
+    console.log();
+
+
+    e.preventDefault();
+}
+
+
+function showError(error) {
+
+
+    document.getElementById('results').style.display = 'none';
+
+    document.getElementById('loading').style.display = 'none';
+    
+    const errorDiv = document.createElement('div');
+
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    errorDiv.className = 'alert alert-danger';
+
+    errorDiv.appendChild(document.createTextNode(error));
+
+    card.insertBefore(errorDiv, heading);
+
+    setTimeout(clearError, 3000);
+
+}
+
+
+function clearError() {
+    document.querySelector('.alert').remove();
+}
